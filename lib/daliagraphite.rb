652ee8d6e4dd5915f54a21fd42dce785860c2be1
Sevastianos Komianos <sebkomianos@nerdvana.gr>
2014-01-20 16:39:37 +0100

    Improve the whole implementation

--- lib/daliagraphite.rb
+++ lib/daliagraphite.rb
@@ -1,5 +1,31 @@
-require "daliagraphite/version"
-require "daliagraphite/blacbox"
+require_relative "daliagraphite/version"
 
-module Daliagraphite
+require "socket"
+
+module Dalia
+end
+
+module Dalia::MiniGraphite
+
+	def self.config(options = {})
+		@@graphite_host = options[:graphite_host]
+		@@graphite_port = options[:graphite_port]
+		@@statsd_host = options[:statsd_host]
+		@@statsd_port = options[:statsd_port]
+	end
+
+	def self.datapoint(key, value, timestamp = Time.now)
+		socket = TCPSocket.new(@@graphite_host, @@graphite_port)
+		signal = "#{key} #{value} #{timestamp.to_i}\n"
+		p signal
+		socket.print(signal)
+		socket.close
+	end
+
+	def self.counter(key, value = 1)
+		socket = UDPSocket.new
+		signal = "#{key}:#{value}|c"
+		socket.send(signal, 0, @@statsd_host, @@statsd_port)
+	end
+
 end
